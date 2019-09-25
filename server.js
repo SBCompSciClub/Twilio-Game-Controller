4ef8d1bb7d750dd43188f715d2d0fe2f4803bd0f
Sethu Senthil <sethunarayansenthil@gmail.com>
2019-09-24 20:09:47 -0400

    Fixed NAN bug, TODO: Fix recursion big

--- server.js
+++ server.js
@@ -4,45 +4,46 @@ const robot = require("robotjs");
 
 let app = express();
 app.use(bodyParser.urlencoded({extended: false}));
-
-var timeCount = 5;
-var rcounter = setInterval(timer, 1000); //round starts every 5 (count) seconds
-console.log(timeCount)
 let active = false; //the round (or voting) is open or not
 // CONTROLS: To add more controls just change these arrays properly
 let counter = [0,0,0,0]  //up down left right  keep track of the respective votes
 let counterMap = ['UP','DOWN','LEFT','RIGHT'] //the text message hotword to trigger those actions
 let keyPressMap = ['up','down','left','right']  //the value that is passed into the keyboard interfacer library check https://robotjs.io/docs/syntax#keyboard
-//END CONTROLS
+//END CONTROLSc
 let highest = -1 //highest value in counter
 let highestIndex = -1 //the index in which (highest) is stored
+var timeCount = 5;
 
-function timer(){
-  active = true
-  timeCount -= 1
-  console.log("the count" + timeCount)
-  if (timeCount <= 0)
+var intervalObj = setInterval(() => intervalNode() , 1000);
+
+function intervalNode(params) {
   {
-     clearInterval(rcounter)
-     active = false
-     for (let i = 0; i < counter.length; i++) { //loop to find the most voted (largest int) int array
-
-      const ele = array[i];
-      if(ele > highest){
-         highest = ele
-         highestIndex = i
-      }
-      else if(ele == highest){ //handle ties
-        if (Math.random() >= 0.5)
-            highestIndex = i
-      }
-    }
-    console.log(`Most of y'all chose to move : ${counterMap[highestIndex]}`)
-    robot.keyTap(keyPressMap[highestIndex])
+    console.log(timeCount)
     active = true
-    var timeCount = 5
-    var rcounter=setInterval(timer, 1000)
-     return;
+    timeCount--
+    console.log("Count:" + timeCount)
+    if (timeCount <= 0)
+    {
+      clearInterval(intervalObj)
+       active = false
+       for (let i = 0; i < counter.length; i++) { //loop to find the most voted (largest int) int array
+
+        const ele = counter[i];
+        if(ele > highest){
+           highest = ele
+           highestIndex = i
+        }
+        else if(ele == highest){ //handle ties
+          if (Math.random() >= 0.5)
+              highestIndex = i
+        }
+      }
+      console.log(`Most of y'all chose to move : ${counterMap[highestIndex]}`)
+      robot.keyTap(keyPressMap[highestIndex])
+      active = true;
+      timeCount = 5;
+      var intervalObj = setInterval(() => intervalNode() , 1000);
+    }
   }
 }
 

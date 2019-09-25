345617f3f11132326661691d9b56eecb733dd4ce
Sethu Senthil <sethunarayansenthil@gmail.com>
2019-09-24 20:56:12 -0400

    :sparks: no more bugs! (hopefully)

--- minml.js
+++ minml.js
@@ -4,7 +4,7 @@ const robot = require("robotjs");
 
 let app = express();
 app.use(bodyParser.urlencoded({extended: false}));
-let active = false; //the round (or voting) is open or not
+let active = true; //the round (or voting) is open or not
 // CONTROLS: To add more controls just change these arrays properly
 let counter = [0,0,0,0]  //up down left right  keep track of the respective votes
 let counterMap = ['UP','DOWN','LEFT','RIGHT'] //the text message hotword to trigger those actions
@@ -13,38 +13,6 @@ let keyPressMap = ['up','down','left','right']  //the value that is passed into 
 let highest = -1 //highest value in counter
 let highestIndex = -1 //the index in which (highest) is stored
 var timeCount = 5;
-
-var intervalObj = setInterval(() => intervalNode() , 1000);
-
-function intervalNode(params) {
-  {
-    console.log(timeCount)
-    active = true
-    timeCount--
-    console.log("Count:" + timeCount)
-    if (timeCount <= 0)
-    {
-       active = false
-       for (let i = 0; i < counter.length; i++) { //loop to find the most voted (largest int) int array
-
-        const ele = counter[i];
-        if(ele > highest){
-           highest = ele
-           highestIndex = i
-        }
-        else if(ele == highest){ //handle ties
-          if (Math.random() >= 0.5)
-              highestIndex = i
-        }
-      }
-      console.log(`Most of y'all chose to move : ${counterMap[highestIndex]}`)
-      robot.keyTap(keyPressMap[highestIndex])
-      counter = [0,0,0,0]
-      active = true
-      timeCount = 5
-    }
-  }
-}
 
 
 
@@ -55,8 +23,12 @@ app.post("/message", function (req, res) {
    const msg = req.body.Body.toUpperCase()
    const succMsg = () => { res.send(`<Response><Message>Your Command "${msg}" was successfully interpreted </Message></Response>`)}
   if(counterMap.includes(msg)){
+    //console.log(counterMap.includes(msg))
+    //console.log(msg)
      let index = counterMap.indexOf(msg)
-     counter[index]++
+     //console.log(index)
+     //console.log(keyPressMap[index])
+     robot.keyTap(keyPressMap[index])
   }else{
     res.send(`<Response><Message>Your Command "${req.body.Body}" is not valid </Message></Response>`)
   }
